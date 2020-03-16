6fd10fc531cccf8532e174796ab94ac6c76f15bb
Derek <dknig43@gmail.com>
2020-03-16 19:48:02 -0400

    updated ui and added new data to get

--- main.py
+++ main.py
@@ -8,29 +8,45 @@ connection = obd.Async()
 
 def new_spd(s):
   string = str(s.value.to('mph'))
-  data = {
-    "speed" : string
-  }
+  data = {"speed" : string}
   print(json.dumps(data))
   sys.stdout.flush()
 
 def new_rpm(r):
   string = str(r.value)
-  data = {
-    "rpm": string
-  }
+  data = {"rpm" : string}
   print(json.dumps(data))
   sys.stdout.flush()
 
+def new_maf(r):
+  string = str(r.value)
+  data = {"maf" : string}
+  print(json.dumps(data))
+  sys.stdout.flush()
+
+def new_fuel(r):
+  string = str(r.value)
+  data = {"fuel" : string}
+  print(json.dumps(data))
+  sys.stdout.flush()
+
+def new_temp(r):
+  string = str(r.value)
+  data = {"temp" : string}
+  print(json.dumps(data))
+  sys.stdout.flush()
 
 # commands to watch
 connection.watch(obd.commands.SPEED, callback=new_spd)
 connection.watch(obd.commands.RPM, callback=new_rpm)
-# connection.watch(obd.commands.FUEL_PRESSURE)
+connection.watch(obd.commands.MAF, callback=new_maf)
+connection.watch(obd.commands.FUEL_LEVEL, callback=new_fuel)
+connection.watch(obd.commands.COOLANT_TEMP, callback=new_temp)
 
 # start the connection
 connection.start()
 
-time.sleep(60)
-connection.stop()
+while True:
+  
 
+connection.stop()
